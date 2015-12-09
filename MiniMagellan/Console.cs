a2c8aadc791cd64444e47e53e6208aeaf881628c
Mike Partain <spiked3@gmail.com>
2015-12-09 14:56:06 -0800

    .

--- MiniMagellan/Console.cs
+++ MiniMagellan/Console.cs
@@ -132,4 +132,27 @@ namespace MiniMagellan
             return Console.ForegroundColor;
         }
     }
+
+    public enum cc { Norm, Good, Bad, Warn, Status }
+
+    // todo trace incoming and outgoing pilot traffic to file
+    public static class Trace
+    {
+        static Dictionary<cc, string> ccDict = new Dictionary<cc, string>();
+
+        static Trace()
+        {
+            ccDict.Add(cc.Good, "^g");
+            ccDict.Add(cc.Bad, "^r");
+            ccDict.Add(cc.Warn, "^y");
+            ccDict.Add(cc.Norm, "^w");
+            ccDict.Add(cc.Status, "^c");
+            //ccDict.Add(cc.??, "^m");
+        }
+
+        public static void t(cc cc, string t)
+        {
+            xCon.WriteLine(ccDict[cc] + t);
+        }
+    }
 }
